9a14d3d53a593e1b1c594a2bacfe2fa0b79de0be
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-14 15:29:34 +0000

    Add 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints digits from 0 to 9 followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
