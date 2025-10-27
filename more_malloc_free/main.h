cff9f794df5f5e3ce44b904af69078a8c352d3b8
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-27 09:39:47 +0000

    Add _calloc function with main and header files

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+#include <stdlib.h>
 
-#endif
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif /* MAIN_H */
