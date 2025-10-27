2b8b8145ee669422c2aa131d9a51405cbe220af6
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-27 18:47:58 +0000

    Add 1-init_dog.c with init_dog function and update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,18 @@
 #define DOG_H
 
 /**
- * struct dog - structure for dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif
-
+#endif /* DOG_H */
