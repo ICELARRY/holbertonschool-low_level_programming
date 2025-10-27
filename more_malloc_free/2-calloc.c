4c75cb81ee352259a90eb8c5d172a637f1d33998
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-27 10:02:50 +0000

    Add _calloc function with main and header files

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,20 +8,20 @@
  *
  * Return: pointer to allocated memory or NULL if failure or nmemb/size is 0
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+void	*_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *ptr;
-    unsigned int i;
+	char	*ptr;
+	unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    ptr = malloc(nmemb * size);
-    if (ptr == NULL)
-        return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
 
-    for (i = 0; i < (nmemb * size); i++)
-        ptr[i] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
-    return ((void *)ptr);
+	return ((void *)ptr);
 }
