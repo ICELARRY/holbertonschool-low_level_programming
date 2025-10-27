e57ab68eba6957005582d6ee86eac65211f312b1
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-27 21:02:16 +0000

    Add main test for dog_t typedef and struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,14 +9,14 @@
  *
  * Description: struct to store dog info
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 
 #endif /* DOG_H */
