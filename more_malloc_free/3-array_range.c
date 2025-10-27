cadc8decb3b118bb061f8f26cf13460d72e427e5
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-27 10:19:55 +0000

    Add prototype for array_range in main.h

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 
 /**
- * array_range - creates an array of integers from min to max inclusive
- * @min: minimum value
- * @max: maximum value
+ * array_range - creates an array of integers from min to max
+ * @min: minimum value in the array
+ * @max: maximum value in the array
  *
  * Return: pointer to the newly created array, or NULL if failure or min > max
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int i, size;
+	int *arr;
+	int i, size;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    size = max - min + 1;
+	size = max - min + 1;
 
-    arr = malloc(sizeof(int) * size);
-    if (arr == NULL)
-        return (NULL);
+	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = min + i;
+	for (i = 0; i < size; i++)
+		arr[i] = min + i;
 
-    return (arr);
+	return (arr);
 }
