010e11f22d41d815ceae24908b9fe32f639af1c8
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-27 09:46:32 +0000

    Add _calloc function with main and header files

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array, using malloc.
- * @nmemb: number of elements.
- * @size: size of each element.
+ * _calloc - allocates memory for an array, initializes to 0
+ * @nmemb: number of elements
+ * @size: size of each element
  *
- * Return: pointer to allocated memory or NULL on failure.
+ * Return: pointer to allocated memory or NULL on failure
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     if (ptr == NULL)
         return (NULL);
 
-    for (i = 0; i < (nmemb * size); i++)
+    for (i = 0; i < nmemb * size; i++)
         ptr[i] = 0;
 
     return ((void *)ptr);
