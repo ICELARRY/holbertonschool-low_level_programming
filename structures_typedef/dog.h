4a8c8217ccf6db1671da59980826996d76797981
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-28 19:16:43 +0000

    Add 5-free_dog.c and update dog.h with free_dog prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,23 @@
 #define DOG_H
 
 /**
- * struct dog - Dog structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - info about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
-typedef struct dog
+struct dog
 {
     char *name;
     float age;
     char *owner;
-} dog_t;
+};
 
-/* Prototype for new_dog function */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);  /* <<< bunu əlavə et */
 
-#endif /* DOG_H */
+#endif
