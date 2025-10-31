731ba16893e1930fc2f664a17dbf413ba2ed28fd
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-10-31 11:23:43 +0000

    Add variadic functions project files

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Prints strings followed by a new line.
+ * @separator: String to print between the strings.
+ * @n: Number of strings passed to the function.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+}
