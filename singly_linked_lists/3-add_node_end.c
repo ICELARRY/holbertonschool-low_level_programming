078bae93fd8b589645d9c950a0f4ae432e401b04
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-03 18:36:54 +0000

    Add prototype for add_node_end in lists.h and implement add_node_end function

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string
+ *
+ * Return: Length
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s && s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to store in the new node
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	if (!head || !str)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_node;
+
+	return (new_node);
+}
